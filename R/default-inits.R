7246b1b06d47f9e573a0bf810ed3ebdb2c9aaf66
jtipton25 <jtipton25@users.noreply.github.com>
2020-05-22 13:42:05 -0500

    Revert "Support shared theta and tau covariance params."

--- R/default-inits.R
+++ R/default-inits.R
@@ -21,10 +21,10 @@ default_inits_pgLM <- function(Y, X, priors) {
 #' @param Y is a \eqn{n \times d}{n x d} matrix of multinomial count data.
 #' @param X is a \eqn{n \times p}{n x p} matrix of variables.
 #' @param priors
-#' @param shared_theta
-#' @param shared_tau
+#' @param corr_fun
+#' @param shared_covariance_params
 #' @noRd
-default_inits_pgSPLM <- function(Y, X, priors, corr_fun = "exponential", shared_theta, shared_tau) {
+default_inits_pgSPLM <- function(Y, X, priors, corr_fun = "exponential", shared_covariance_params = TRUE) {
 
     J <- ncol(Y)
     theta_mean <- NULL
@@ -39,12 +39,12 @@ default_inits_pgSPLM <- function(Y, X, priors, corr_fun = "exponential", shared_
     
     inits <- list(
         beta  = t(mvnfast::rmvn(J-1, priors$mu_beta, priors$Sigma_beta)),
-        tau2  = if (shared_tau) {
+        tau2  = if (shared_covariance_params) {
             min(1 / rgamma(1, priors$alpha_tau, priors$beta_tau), 10)
         } else {
             pmin(1 / rgamma(J-1, priors$alpha_tau, priors$beta_tau), 10)
         },
-        theta = if (shared_theta) {
+        theta = if (shared_covariance_params) {
             if (corr_fun == "matern") {
                 as.vector(pmin(pmax(mvnfast::rmvn(1, theta_mean, theta_var), -1), 0.1))
             } else if (corr_fun == "exponential") {
